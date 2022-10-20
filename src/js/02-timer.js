d98d045d60561a85af04eb8c5c588f4ecc4c7c21
nickgric <nick.gric@gmail.com>
2022-10-20 15:32:06 +0300

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const startBtn = document.querySelector('[data-start]');
-const timeInput = document.querySelector('[datetime-picker]');
+const timeInput = document.querySelector('#datetime-picker');
 
 const labelDays = document.querySelector('[data-days]');
 const labelHours = document.querySelector('[data-hours]');
@@ -16,8 +16,10 @@ const labels = document.querySelectorAll('.label');
 const fields = document.querySelectorAll('.field');
 
 timer.style.display = 'flex';
-timer.style.gap = '10px';
+timer.style.gap = '20px';
 timer.style.marginTop = '20px';
+
+startBtn.disabled = true;
 
 values.forEach(value => {
   value.style.fontSize = '40px';
@@ -35,4 +37,69 @@ fields.forEach(field => {
   field.style.flexDirection = 'column';
 });
 
-console.log(fields);
+let currentDate = new Date();
+let timerMs = 0;
+let timerObj = {};
+
+const flatpickrOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timerMs = +Date.parse(selectedDates[0]) - +Date.parse(currentDate);
+    if (timerMs <= 0) {
+      Notify.failure('Please choose a date in the future!');
+      return;
+    }
+
+    startBtn.disabled = false;
+  },
+};
+
+flatpickr('#datetime-picker', flatpickrOptions);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function timerUpdater() {
+  labelsUpdater();
+  Notify.success('The timer has started');
+  setInterval(labelsUpdater, 1000);
+}
+
+function labelsUpdater() {
+  startBtn.disabled = true;
+  timeInput.disabled = true;
+
+  if (timerMs < 0) {
+    Notify.success('Timer has expired');
+    return;
+  }
+
+  timerObj = convertMs(timerMs);
+
+  labelDays.textContent = addLeadingZero(timerObj.days);
+  labelHours.textContent = addLeadingZero(timerObj.hours);
+  labelMinutes.textContent = addLeadingZero(timerObj.minutes);
+  labelSeconds.textContent = addLeadingZero(timerObj.seconds);
+
+  timerMs = timerMs - 1000;
+}
+
+startBtn.addEventListener('click', timerUpdater);
